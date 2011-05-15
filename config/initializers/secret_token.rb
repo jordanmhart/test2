ddb8140a23eb9947901feab6e1c46958339d5ec1
madidas <madidas@madidas-virtualbox.(none)>
2011-05-14 21:16:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test2::Application.config.secret_token = 'bbe55b0ce3dbbc02b9c4b4f3ba70a8142d03b0f053c41257f865eed5e9fac7bf200c2cf303c9d074a41a3af0179a2b4f107c5b31da0c7406535e41a2f82e3580'
